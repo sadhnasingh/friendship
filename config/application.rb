80f1944be5f95e267229e1439bdb16cec7d39b03
sadhna <sadhna@bittern.co>
2018-08-10 17:44:11 +0530

    add

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require_relative 'boot'
 
 require 'rails/all'
-
+require 'devise'
+require 'carrierwave'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
